833e4b874025558a86492e5a919e3b8e5d328d47
Johannes Schickling <schickling.j@gmail.com>
2013-10-21 23:35:02 +0200

    Added animation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,8 @@
-console.log('\'Allo \'Allo!');
+var activeStepCount = 1,
+	$listElements = $('#howto li');
+
+setInterval(function() {
+	$listElements.eq(activeStepCount).addClass('active').siblings().removeClass('active');
+	activeStepCount++;
+	activeStepCount %= 4;
+}, 2000);
